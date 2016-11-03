ad311877f0ea2845a5dd8f06e44fe0c1d673f0f6
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-11-03 22:29:16 +0900

    #05 all,first,last,find,find_by,find_by_andメソッド

--- main.rb
+++ main.rb
@@ -29,6 +29,26 @@ end
 # post.save
 
 # 書き方⑤（newとsaveを同時にやってくれる書き方）
-Post.create(:title => "title1", :body => "hello1")
+# Post.create(:title => "title1", :body => "hello1")
 
-p Post.all
+
+
+# DB内のすべての値を取ってくる
+# p Post.all
+
+# DB内の最初のレコードをとっってくる
+# p Post.first
+
+# 一番最後に挿入したtitleレコードを呼び出したい時
+# p Post.last.title
+
+# IDを指定して呼び出したい時：findメソッド
+# p Post.find(3)
+
+#特定のフィールド名を指定して呼び出したい時：find_by_フィールド名メソッド
+# p Post.find_by_title("title2")
+
+#複数の条件で特定のフィールドを呼び出したい時：find_by_レコード名_and_レコード名（" フィールド名", "フィールド名"）
+p Post.find_by_title_and_id("title2", 2)
+
+# ※もし該当するフィールドがなければ、nilが返ってくる
