2d851cd73069ae56d60012d17ec98839cf7af907
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-11-03 23:13:06 +0900

    #07 order, limit, scope, logger

--- main.rb
+++ main.rb
@@ -1,29 +1,37 @@
 require 'active_record'
 
+#どんなSQLが発行されているかターミナルでみれる
+require 'logger'
+
+
 ActiveRecord::Base.establish_connection(
   "adapter" => "sqlite3",
   "database" => "./blog.db"
 )
 
+#どんなSQLが発行されているかターミナルでみれる（上記の記述と合わせて使用する）
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+
 class Post < ActiveRecord::Base
+  scope :top3, -> {order("created_at").limit(3)}
 end
 
 
-# idとtitleを指定して呼び出す書き方
-# p Post.where(:title => "title1", :id => 1)
+# idの１〜３を抽出したい時
+# p Post.where(:id => 1..3)
 
 
-# 最初に？マークを使ってレコードを指定。続いて、その中で取り出したいフィールド名を指定する書き方
-# p Post.where("title = ? and id = ?", "title1", 1)
+# idの１と３を飛び飛びに抽出したい時：配列構造にする
+# p Post.where(:id => [1, 3])
 
 
-# シンボルを使った記述方法（どれがどれに対応しているか分かりやすくする）
-# p Post.where("title= :title and id = :id", {:title => "title1", :id => 1})
+# idを降順に並び替え、上から3つ目までを抽出したい時。
+# p Post.order("id desc").limit(3)
 
 
-#不等号条件を指定
-# p Post.where("id > ?", 2)
+# scopeで指定された範囲で、指定された条件のレコードを抽出する
+p Post.top3
 
 
-# 全レコードが選択される
-p Post.where("body like ?", "hello%")
+
