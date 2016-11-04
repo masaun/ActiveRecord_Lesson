7ae7e76e7ca38b7fe978e1bf5593330ae65e18d5
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-11-04 22:43:19 +0900

    #08 first_or_create

--- main.rb
+++ main.rb
@@ -14,24 +14,20 @@ ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 
 class Post < ActiveRecord::Base
-  scope :top3, -> {order("created_at").limit(3)}
 end
 
 
-# idの１〜３を抽出したい時
-# p Post.where(:id => 1..3)
+# first_or_createメソッドを使うと、指定したフィールド名が存在する場合はその値を抽出。存在しない場合は新しく作ってくれる。
+# Post.where(:title => "title5").first_or_create
+# p Post.all
 
 
-# idの１と３を飛び飛びに抽出したい時：配列構造にする
-# p Post.where(:id => [1, 3])
+# もし指定したフィールド名が存在する場合はその値を抽出。存在しない場合はpの中でbodyの値も作ってくれる。。
+Post.where(:title => "title6").first_or_create do |p|
+  p.body = "hello6"
+end
 
-
-# idを降順に並び替え、上から3つ目までを抽出したい時。
-# p Post.order("id desc").limit(3)
-
-
-# scopeで指定された範囲で、指定された条件のレコードを抽出する
-p Post.top3
+p Post.all
 
 
 
